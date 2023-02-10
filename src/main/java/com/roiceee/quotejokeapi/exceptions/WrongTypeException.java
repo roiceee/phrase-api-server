7c9a5020cd98022c1fb670e4df8b4b329fd79ab4
jroiceee <jroicealdeza@gmail.com>
2023-02-10 17:30:37 +0800

    Handled the exception message on the exception class itself

--- src/main/java/com/roiceee/quotejokeapi/exceptions/WrongTypeException.java
+++ src/main/java/com/roiceee/quotejokeapi/exceptions/WrongTypeException.java
@@ -1,4 +1,14 @@
 package com.roiceee.quotejokeapi.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class WrongTypeException extends RuntimeException{
+    public WrongTypeException(String message) {
+        super(message);
+    }
+    public WrongTypeException() {
+        super("Invalid value for request parameter 'type'.");
+    }
 }
