8a905345a00e558844c6ffac24d531ed590f9123
jroiceee <jroicealdeza@gmail.com>
2023-02-12 01:44:28 +0800

    implement integration testing

--- src/main/java/com/roiceee/quotejokeapi/exceptions/InvalidParamQuantityValueException.java
+++ src/main/java/com/roiceee/quotejokeapi/exceptions/InvalidParamQuantityValueException.java
@@ -1,8 +1,13 @@
 package com.roiceee.quotejokeapi.exceptions;
 
 import com.roiceee.quotejokeapi.util.ReqParamNames;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidParamQuantityValueException extends RuntimeException{
+
 
     public InvalidParamQuantityValueException(int quantity) {
         super("Invalid value '" + quantity + "' for '" + ReqParamNames.qty + "'.");
--- src/test/java/com/roiceee/quotejokeapi/RequestResourceControllerIntegrationTest.java
+++ src/test/java/com/roiceee/quotejokeapi/RequestResourceControllerIntegrationTest.java
@@ -1,8 +1,16 @@
 package com.roiceee.quotejokeapi;
 
+import com.roiceee.quotejokeapi.controllers.RequestResourceController;
+import com.roiceee.quotejokeapi.services.FetchResourceService;
+import com.roiceee.quotejokeapi.util.ReqParamNames;
 import com.roiceee.quotejokeapi.util.ReqParamTypeValues;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,17 +22,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class RequestResourceControllerIntegrationTest {
 
     @Autowired
     MockMvc mockMvc;
 
-
     @Test
     @Description("Check status of getRandomResource with parameter 'type' value of 'joke'.")
     public void getRandomJokeResourceTestHappyFlowTestStatus() throws Exception {
         String type = ReqParamTypeValues.JOKE;
-        mockMvc.perform(get("/api").param("type", type))
+        mockMvc.perform(get("/api").param(ReqParamNames.type, type))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
@@ -33,9 +41,35 @@ public class RequestResourceControllerIntegrationTest {
     @Description("Check status of getRandomJokeResource with wrong parameter 'type' value.")
     public void getRandomJokeResourceTestWrongTypeValueTestStatus() throws Exception {
         String type = "wrong type";
-        mockMvc.perform(get("/api").param("type", type))
+        mockMvc.perform(get("/api").param(ReqParamNames.type, type))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+    }
+    @Test
+    @Description("Check status of getRandomResourceList with " +
+            "parameter type = joke and quantity = 3.")
+    public void getRandomJokeResourceListHappyFlowTestStatus() throws Exception {
+        String type = ReqParamTypeValues.JOKE;
+        int qty = 3;
+        mockMvc.perform(get("/api").param(ReqParamNames.type, type)
+                        .param(ReqParamNames.qty, String.valueOf(qty)))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    @Description("Check status of getRandomResourceList with" +
+            "parameter type = joke and wrong quantity")
+    public void getRandomJokeResourceListWrongTypeValueTestStatus() throws Exception {
+        String type = ReqParamTypeValues.JOKE;
+        int qty = 11;
+        mockMvc.perform(get("/api").param(ReqParamNames.type, type)
+                        .param(ReqParamNames.qty, String.valueOf(qty)))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
+
+
 
 }
