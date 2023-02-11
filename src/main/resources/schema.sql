f377bcb80a7bf73e2ebff124754c99053cb4073c
jroiceee <jroicealdeza@gmail.com>
2023-02-11 13:09:17 +0800

    clean schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,14 +2,12 @@ CREATE SCHEMA IF NOT EXISTS`quote-joke-api` ;
 
 CREATE TABLE IF NOT EXISTS`quote-joke-api`.`jokes` (
                                                        `id` BIGINT NOT NULL AUTO_INCREMENT,
-                                                       `joke` MEDIUMTEXT NOT NULL,
+                                                       `phrase` MEDIUMTEXT NOT NULL,
                                                        PRIMARY KEY (`id`));
 
-ALTER TABLE `quote-joke-api`.`jokes`
-    CHANGE COLUMN `joke` `phrase` MEDIUMTEXT NOT NULL ;
 
 CREATE TABLE IF NOT EXISTS`quote-joke-api`.`quotes` (
                                                         `id` BIGINT NOT NULL AUTO_INCREMENT,
                                                         `author` VARCHAR(45) NOT NULL,
-                                                        `quote` MEDIUMTEXT NOT NULL,
+                                                        `phrase` MEDIUMTEXT NOT NULL,
                                                         PRIMARY KEY (`id`));
