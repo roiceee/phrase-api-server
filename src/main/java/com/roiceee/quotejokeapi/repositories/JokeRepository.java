84a090dbe1cbeef5af8e3c9cf5de4d0f1df98b50
jroiceee <jroicealdeza@gmail.com>
2023-02-12 01:15:43 +0800

    implement retrieving random phrase lists

--- src/main/java/com/roiceee/quotejokeapi/repositories/JokeRepository.java
+++ src/main/java/com/roiceee/quotejokeapi/repositories/JokeRepository.java
@@ -2,8 +2,12 @@ package com.roiceee.quotejokeapi.repositories;
 
 import com.roiceee.quotejokeapi.models.JokeModel;
 
+import com.roiceee.quotejokeapi.models.Phrase;
+import com.roiceee.quotejokeapi.models.QuoteModel;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface JokeRepository extends CrudRepository<JokeModel, Long> {
 
@@ -12,4 +16,9 @@ public interface JokeRepository extends CrudRepository<JokeModel, Long> {
             "ORDER BY RAND() LIMIT  1",
             nativeQuery = true)
     JokeModel getRandomJoke();
+
+    @Query(value = "SELECT * FROM jokes " +
+            "ORDER BY RAND() LIMIT  :quantity",
+            nativeQuery = true)
+    List<JokeModel> getRandomJokeList(int quantity);
 }
--- src/main/java/com/roiceee/quotejokeapi/repositories/QuoteRepository.java
+++ src/main/java/com/roiceee/quotejokeapi/repositories/QuoteRepository.java
@@ -1,10 +1,13 @@
 package com.roiceee.quotejokeapi.repositories;
 
 import com.roiceee.quotejokeapi.models.JokeModel;
+import com.roiceee.quotejokeapi.models.Phrase;
 import com.roiceee.quotejokeapi.models.QuoteModel;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuoteRepository extends CrudRepository<QuoteModel, Long> {
@@ -14,5 +17,10 @@ public interface QuoteRepository extends CrudRepository<QuoteModel, Long> {
                     "ORDER BY RAND() LIMIT  1",
             nativeQuery = true)
     QuoteModel getRandomQuote();
+
+    @Query(value = "SELECT * FROM quotes " +
+            "ORDER BY RAND() LIMIT  :quantity",
+            nativeQuery = true)
+    List<QuoteModel> getRandomQuoteList(int quantity);
 
 }
