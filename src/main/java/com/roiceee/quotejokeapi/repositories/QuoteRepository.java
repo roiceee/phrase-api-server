21120d743ae2f89c6920a85b07ab6ccbb5718e7f
jroiceee <jroicealdeza@gmail.com>
2023-02-11 12:47:13 +0800

    add QuoteRepository

---
+++ src/main/java/com/roiceee/quotejokeapi/repositories/QuoteRepository.java
@@ -0,0 +1,18 @@
+package com.roiceee.quotejokeapi.repositories;
+
+import com.roiceee.quotejokeapi.models.JokeModel;
+import com.roiceee.quotejokeapi.models.QuoteModel;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuoteRepository extends CrudRepository<QuoteModel, Long> {
+
+    @Query(value =
+            "SELECT * FROM quotes " +
+                    "ORDER BY RAND() LIMIT  1",
+            nativeQuery = true)
+    JokeModel getRandomQuote();
+
+}
