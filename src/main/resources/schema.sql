1b390414c21d397d311e099a014fc19c7ceb131d
jroiceee <jroicealdeza@gmail.com>
2023-02-11 12:34:38 +0800

    add quotes table script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,9 @@ CREATE TABLE IF NOT EXISTS`quote-joke-api`.`jokes` (
 
 ALTER TABLE `quote-joke-api`.`jokes`
     CHANGE COLUMN `joke` `phrase` MEDIUMTEXT NOT NULL ;
+
+CREATE TABLE IF NOT EXISTS`quote-joke-api`.`quotes` (
+                                                        `id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                        `author` VARCHAR(45) NOT NULL,
+                                                        `quote` MEDIUMTEXT NOT NULL,
+                                                        PRIMARY KEY (`id`));
