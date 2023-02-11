eb7a97d1ae9608db8ffcc8f4ab35f93549c3bed4
jroiceee <jroicealdeza@gmail.com>
2023-02-12 01:14:28 +0800

    implement getRandomPhraseList

--- src/main/java/com/roiceee/quotejokeapi/services/FetchResourceService.java
+++ src/main/java/com/roiceee/quotejokeapi/services/FetchResourceService.java
@@ -1,12 +1,17 @@
 package com.roiceee.quotejokeapi.services;
 
-import com.roiceee.quotejokeapi.exceptions.WrongReqParamTypeException;
+import com.roiceee.quotejokeapi.exceptions.InvalidParamQuantityValueException;
+import com.roiceee.quotejokeapi.exceptions.InvalidParamTypeValueException;
+import com.roiceee.quotejokeapi.models.JokeModel;
 import com.roiceee.quotejokeapi.models.Phrase;
+import com.roiceee.quotejokeapi.models.QuoteModel;
 import com.roiceee.quotejokeapi.repositories.JokeRepository;
 import com.roiceee.quotejokeapi.repositories.QuoteRepository;
 import com.roiceee.quotejokeapi.util.ReqParamTypeValues;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -21,18 +26,26 @@ public class FetchResourceService {
 
     public Phrase getRandomPhrase(String type) {
         return switch (type) {
-            case ReqParamTypeValues.JOKE -> getRandomJoke();
-            case ReqParamTypeValues.QUOTE -> getRandomQuote();
-            default -> throw new WrongReqParamTypeException(type);
+            case ReqParamTypeValues.JOKE -> jokeRepository.getRandomJoke();
+            case ReqParamTypeValues.QUOTE -> quoteRepository.getRandomQuote();
+            default -> throw new InvalidParamTypeValueException(type);
         };
     }
 
-    private Phrase getRandomJoke() {
-        return jokeRepository.getRandomJoke();
+    public List<? extends Phrase> getRandomPhraseList(String type, int quantity) {
+        if (quantity < ReqParamTypeValues.MINQTY ||
+                quantity > ReqParamTypeValues.MAXQTY) {
+            throw new InvalidParamQuantityValueException(quantity);
+        }
+
+        return switch (type) {
+            case ReqParamTypeValues.JOKE ->
+                    jokeRepository.getRandomJokeList(quantity);
+            case ReqParamTypeValues.QUOTE ->
+                    quoteRepository.getRandomQuoteList(quantity);
+            default -> throw new InvalidParamTypeValueException(type);
+        };
     }
 
-    private Phrase getRandomQuote() {
-        return quoteRepository.getRandomQuote();
-    }
 
 }
