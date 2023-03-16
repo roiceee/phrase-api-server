f2b838eb204576be82532a49da3639985ab7e660
jroiceee <jroicealdeza@gmail.com>
2023-03-16 22:28:22 +0800

    update schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,19 @@
 CREATE SCHEMA IF NOT EXISTS`phrase-api` ;
 
-CREATE TABLE IF NOT EXISTS`phrase-api`.`jokes` (
-                                                       `id` BIGINT NOT NULL AUTO_INCREMENT,
-                                                       `phrase` MEDIUMTEXT NOT NULL,
-                                                       PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `jokes` (
+                         `id` int DEFAULT NULL,
+                         `phrase` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
 
-CREATE TABLE IF NOT EXISTS`phrase-api`.`quotes` (
-                                                        `id` BIGINT NOT NULL AUTO_INCREMENT,
-                                                        `author` VARCHAR(45) NOT NULL,
-                                                        `phrase` MEDIUMTEXT NOT NULL,
-                                                        PRIMARY KEY (`id`));
+CREATE TABLE IF NOT EXISTS `quotes` (
+                          `id` int DEFAULT NULL,
+                          `author` text,
+                          `phrase` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+CREATE TABLE IF NOT EXISTS `api_keys` (
+                            `id` varchar(255) NOT NULL,
+                            `api_key` varchar(255) DEFAULT NULL,
+                            PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
