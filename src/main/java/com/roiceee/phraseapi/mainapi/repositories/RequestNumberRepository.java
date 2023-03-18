39935a87090bce9f6a3126ffd8c869c94108831d
jroiceee <jroicealdeza@gmail.com>
2023-03-18 12:55:39 +0800

    fix request count functionality

--- src/main/java/com/roiceee/phraseapi/mainapi/repositories/RequestNumberRepository.java
+++ src/main/java/com/roiceee/phraseapi/mainapi/repositories/RequestNumberRepository.java
@@ -1,20 +1,19 @@
 package com.roiceee.phraseapi.mainapi.repositories;
 
 import com.roiceee.phraseapi.mainapi.models.RequestCountModel;
-<<<<<<< HEAD
+
 import org.springframework.data.jpa.repository.Modifying;
-=======
->>>>>>> origin/main
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RequestNumberRepository extends CrudRepository<RequestCountModel, Long> {
 
-<<<<<<< HEAD
-    @Modifying
-=======
 
->>>>>>> origin/main
+    @Modifying
+
+
+
     @Query(value = "INSERT INTO request_count (request_number, api_Key) values (null, :apiKey);", nativeQuery =
             true)
     void addRequestCount(String apiKey);
