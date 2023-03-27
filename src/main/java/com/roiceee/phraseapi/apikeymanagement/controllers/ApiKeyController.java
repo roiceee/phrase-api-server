e284802bb2dd59f92dcc40d87b32d1574ffa78be
jroiceee <jroicealdeza@gmail.com>
2023-03-27 22:10:46 +0800

    configure cors

--- src/main/java/com/roiceee/phraseapi/apikeymanagement/controllers/ApiKeyController.java
+++ src/main/java/com/roiceee/phraseapi/apikeymanagement/controllers/ApiKeyController.java
@@ -2,26 +2,32 @@ package com.roiceee.phraseapi.apikeymanagement.controllers;
 
 import com.roiceee.phraseapi.apikeymanagement.models.UserApiKeyModel;
 import com.roiceee.phraseapi.apikeymanagement.services.ApiKeyService;
+import com.roiceee.phraseapi.services.ResourceControllerLimiterService;
+import com.roiceee.phraseapi.util.Origins;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {Origins.LOCAL, Origins.PROD})
 @RequestMapping("apikey")
 public class ApiKeyController {
 
     ApiKeyService apiKeyService;
+    ResourceControllerLimiterService resourceControllerLimiterService;
 
-    public ApiKeyController(ApiKeyService apiKeyService) {
+    public ApiKeyController(ApiKeyService apiKeyService, ResourceControllerLimiterService resourceControllerLimiterService) {
         this.apiKeyService = apiKeyService;
+        this.resourceControllerLimiterService = resourceControllerLimiterService;
     }
 
     @PostMapping("create")
     public ResponseEntity<UserApiKeyModel> createAPIKey(Authentication authentication) {
 
         UserApiKeyModel userApiKeyModel = apiKeyService.createNewApiKey(authentication.getName());
-
+        resourceControllerLimiterService.addExistingKeyIfAbsentInCache(userApiKeyModel.getApiKey());
         return ResponseEntity.ok()
                 .body(userApiKeyModel);
     }
@@ -33,7 +39,8 @@ public class ApiKeyController {
 
     @DeleteMapping("delete")
     public ResponseEntity<String> deleteApiKey(Authentication authentication) {
-        apiKeyService.deleteApiKey(authentication.getName());
+        Optional<UserApiKeyModel> keyModel = apiKeyService.deleteApiKey(authentication.getName());
+        keyModel.ifPresent(userApiKeyModel -> resourceControllerLimiterService.deleteFromCache(userApiKeyModel.getApiKey()));
         return ResponseEntity.ok().body("API key deleted.");
     }
 }
---
+++ src/main/java/com/roiceee/phraseapi/util/Origins.java
@@ -0,0 +1,6 @@
+package com.roiceee.phraseapi.util;
+
+public class Origins {
+    public static final String LOCAL = "http://localhost:3000";
+    public static final String PROD = "https://phraseapi.vercel.app";
+}
