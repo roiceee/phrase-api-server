d85530fac4ef1d00ffafdd02be9342fc93da9a38
jroiceee <jroicealdeza@gmail.com>
2023-06-22 20:15:36 +0800

    change repository interface

--- src/main/java/com/roiceee/phraseapi/resourceapi/repository/JokeRepository.java
+++ src/main/java/com/roiceee/phraseapi/resourceapi/repository/JokeRepository.java
@@ -4,12 +4,13 @@ import com.roiceee.phraseapi.resourceapi.model.JokeModel;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface JokeRepository extends PagingAndSortingRepository<JokeModel, Long> {
+public interface JokeRepository extends JpaRepository<JokeModel, Long> {
 
     @Query(value =
             "SELECT * FROM public.jokes " +
--- src/main/java/com/roiceee/phraseapi/resourceapi/repository/QuoteRepository.java
+++ src/main/java/com/roiceee/phraseapi/resourceapi/repository/QuoteRepository.java
@@ -3,6 +3,7 @@ package com.roiceee.phraseapi.resourceapi.repository;
 import com.roiceee.phraseapi.resourceapi.model.QuoteModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -10,7 +11,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 
-public interface QuoteRepository extends PagingAndSortingRepository<QuoteModel, Long> {
+public interface QuoteRepository extends JpaRepository<QuoteModel, Long> {
 
     @Query(value =
             "SELECT * FROM public.quotes " +
