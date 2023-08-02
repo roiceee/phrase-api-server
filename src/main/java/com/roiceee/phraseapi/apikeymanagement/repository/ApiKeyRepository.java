bb6c18dff5e68671c61d3e8f41d2ba87507fd8b0
jroiceee <jroicealdeza@gmail.com>
2023-08-03 00:42:51 +0800

    refactor

--- src/main/java/com/roiceee/phraseapi/apikeymanagement/repository/ApiKeyRepository.java
+++ src/main/java/com/roiceee/phraseapi/apikeymanagement/repository/ApiKeyRepository.java
@@ -5,17 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 
 public interface ApiKeyRepository extends JpaRepository<UserApiKeyModel, String> {
 
     @Modifying
     @Query(value = "INSERT INTO public.api_keys (id, api_key) VALUES (:id, :apiKey);", nativeQuery = true)
     void createApiKey(String id, String apiKey);
-
-    @Query(value = "SELECT * FROM public.api_keys;", nativeQuery = true)
-    List<UserApiKeyModel> getAll();
 
     UserApiKeyModel findByApiKey(String apiKey);
 
