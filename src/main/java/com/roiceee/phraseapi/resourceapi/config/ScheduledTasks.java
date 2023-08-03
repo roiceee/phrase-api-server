e9cacf6dffe8569facacffa8fbb5981bc484bee3
jroiceee <jroicealdeza@gmail.com>
2023-08-03 13:10:17 +0800

    temporarily remove rate limiter per api request to monitor usage

--- src/main/java/com/roiceee/phraseapi/resourceapi/config/ScheduledTasks.java
+++ src/main/java/com/roiceee/phraseapi/resourceapi/config/ScheduledTasks.java
@@ -1,24 +1,20 @@
 package com.roiceee.phraseapi.resourceapi.config;
 
-import com.roiceee.phraseapi.resourceapi.service.ResourceControllerLimiterService;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
-
 //temporary remove rate limiter to monitor memory usage
 //@Configuration
 //@EnableScheduling
 public class ScheduledTasks {
-
-    private final ResourceControllerLimiterService resourceControllerLimiterService;
-
-    public ScheduledTasks(ResourceControllerLimiterService resourceControllerLimiterService) {
-        this.resourceControllerLimiterService = resourceControllerLimiterService;
-    }
-
-    //delay is 6 hours = 21600000 milliseconds
-    @Async
-    @Scheduled(initialDelay = 21600000, fixedDelay = 21600000)
-    public void clearResourceControllerLimiterServiceCache() {
-        resourceControllerLimiterService.clearUnusedKeysFromCache();
-    }
+//
+//    private final ResourceControllerLimiterService resourceControllerLimiterService;
+//
+//    public ScheduledTasks(ResourceControllerLimiterService resourceControllerLimiterService) {
+//        this.resourceControllerLimiterService = resourceControllerLimiterService;
+//    }
+//
+//    //delay is 6 hours = 21600000 milliseconds
+//    @Async
+//    @Scheduled(initialDelay = 21600000, fixedDelay = 21600000)
+//    public void clearResourceControllerLimiterServiceCache() {
+//        resourceControllerLimiterService.clearUnusedKeysFromCache();
+//    }
 }
